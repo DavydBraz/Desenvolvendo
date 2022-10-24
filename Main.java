c8af28e8bb0e4d44106f91e19401dfd25072c2bc
Davyd Braz <102470929+DavydBraz@users.noreply.github.com>
2022-10-24 10:35:59 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,14 +1,11 @@
-class Main {
+public class Main{
   public static void main(String[] args) {
-    String[] HD_Do_Programa = { "0-5", "1-5", "2-4", "3-0", "4-10", "5-2", "6-8", "7-3" };// primeiro valor é o endereço
-                                                                                          // e o segundo é o valor
+		//EXEMPLO QUE COMO USAR A FABRICA DE ENTRADAS
+		//Memoria Virtual Minima = 10
+		//Memoria Virtual Maxima = 40
+		int tamanhoDaMinhaMemoriaVirtual = 10;
+		String SUA_ENTRADA = new FabricaDeEntradas(tamanhoDaMinhaMemoriaVirtual).getNewEntrada();
+		System.out.println(SUA_ENTRADA);
+	}
 
-    String[] MemVirtual = { null, null, null, null, null, null, null, null };
-    // espaço de endereçamento eu acho, no caso de 8 regioes, as quais a memoria
-    // fisica só vai ter 4
-    String[] MemFisica = { null, null, null, null };
-    // da para abstrair como se os programas ocupassem o mesmo espaço, dessa forma
-    // justificaria pq não vou usar especificações de tamanhos e mudanças de formas
-    // de aproveitar esse espaço
-  }
 }
