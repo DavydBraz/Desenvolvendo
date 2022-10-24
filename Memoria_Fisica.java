ac0c70cc3beb322bf6a4854bd8a5e7f63d008ef3
Davyd Braz <102470929+DavydBraz@users.noreply.github.com>
2022-10-24 10:38:46 -0300

    Create Memoria_Fisica.java

---
+++ Memoria_Fisica.java
@@ -0,0 +1,5 @@
+public class Memoria_Fisica{
+  int tamanho_f;
+  //daria para por para ambas memorias heradarem de memoria ja que possui alguns elementos em comum, tipo o tamanho
+  //quadro de pagina(correspondente da pagina virtual)
+}
