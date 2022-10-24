45077f224031ca67cc45b207c22bdffaedc907f1
Davyd Braz <102470929+DavydBraz@users.noreply.github.com>
2022-10-24 10:40:32 -0300

    Create Memoria_Virtual.java

---
+++ Memoria_Virtual.java
@@ -0,0 +1,7 @@
+import java.util.ArrayList;
+
+public class Memoria_Virtual {
+  int tamanho;
+  Tabela_Pagina Tabela;
+  //dai ela teria um tamanho que seria o dobro da memoria fisica, e utilizaria a tabela de paginas para referenciar posições a serem utilizadas na memoria fisica
+}
